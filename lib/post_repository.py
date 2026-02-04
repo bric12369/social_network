32a550196e44c1314fa9e2126d7cc84c497bde69
Barry <barryrichards05@gmail.com>
2026-02-04 16:14:30 +0000

    added create method to post repository

--- lib/post_repository.py
+++ lib/post_repository.py
@@ -11,3 +11,14 @@ class PostRepository:
             post = Post(row['id'], row['title'], row['content'], row['views'], row['user_id'])
             posts.append(post)
         return posts
+    
+    def create(self, post_instance):
+        self._connection.execute('INSERT INTO posts (title, content, views, user_id) VALUES (%s, %s, %s, %s)', [post_instance.title, post_instance.content, post_instance.views, post_instance.user_id])
+
+
+    # id SERIAL PRIMARY KEY,
+    # title TEXT,
+    # content TEXT,
+    # views INT,
+    # user_id INT REFERENCES user_accounts(id)
+    # on delete cascade
--- tests/test_post_repository.py
+++ tests/test_post_repository.py
@@ -9,3 +9,15 @@ def test_lists_all(db_connection):
         Post(2, 'Football', '2-0 to West Ham!', 5, 2),
         Post(3, 'Gym', 'Just got a new PB on bench!', 7, 3)
     ]
+
+def test_create_post(db_connection):
+    db_connection.seed('seeds/social_network.sql')
+    repository = PostRepository(db_connection)
+    new_post = Post(None, 'New', 'This is a new post', 5, 1)
+    repository.create(new_post)
+    assert repository.all() == [
+        Post(1, 'WINDY', 'It is windy today.', 3, 1),
+        Post(2, 'Football', '2-0 to West Ham!', 5, 2),
+        Post(3, 'Gym', 'Just got a new PB on bench!', 7, 3),
+        Post(4, 'New', 'This is a new post', 5, 1)
+    ]
