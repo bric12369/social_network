31407faf1e49eebe24e33f835644238c7cd03e35
Barry <barryrichards05@gmail.com>
2026-02-05 12:15:57 +0000

    added application class

--- app.py
+++ app.py
@@ -2,17 +2,29 @@ from lib.database_connection import *
 from lib.user_account_repository import *
 from lib.post_repository import *
 
-connection = DatabaseConnection()
-print(connection.connect())
-print(connection.seed('seeds/social_network.sql'))
+class Application:
+    def __init__(self):
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        self._connection.seed('seeds/social_network.sql')
 
-user_repo = UserAccountRepository(connection)
-post_repo = PostRepository(connection)
+    def run(self):
+        print('Welcome to the social network! \n')
 
-users = user_repo.all()
-[print(user) for user in users]
+        choice = input('What would you like to do? \n  1 - List users \n  2 - List posts \n')
 
-print('\n')
+        user_repo = UserAccountRepository(self._connection)
+        users = user_repo.all()
+        post_repo = PostRepository(self._connection)
+        posts = post_repo.all()
 
-posts = post_repo.all()
-[print(post) for post in posts]
+        if choice == '1':
+            [print(user.username) for user in users]
+        elif choice == '2':
+            [print(f'{post.title}: {post.content}') for post in posts]
+        else:
+            print('Invalid choice.')
+
+if __name__ == '__main__':
+    app = Application()
+    app.run()
